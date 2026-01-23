4eb72ab793d4e2b047f320837f954579c9c718b2
ptrodion <rdnpetrenko@gmail.com>
2026-01-23 22:39:56 +0000

    Added hero

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import { loadPartial } from './js/loadPartials.js';
 
 loadPartial('#header', './partials/header.html');
 loadPartial('#footer', './partials/footer.html');
+loadPartial('#hero', './partials/main/hero.html');
