b004db0a3a975d10417fbfbed29c1c8ffd53ca6c
ptrodion <rdnpetrenko@gmail.com>
2026-01-26 23:23:36 +0000

    Added 1 part of post and start ti use BAM system for classes

--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@ import { loadPartial } from './js/loadPartials.js';
 loadPartial('#header', './partials/header.html');
 loadPartial('#footer', './partials/footer.html');
 loadPartial('#hero', './partials/main/hero.html');
+loadPartial('#post', './partials/main/post.html');
